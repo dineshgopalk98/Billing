5e5625c5380c435b42d166077b7e0751364093c3
Dinesh Krishnan <dineshkrishnan7998@gmail.com>
2025-07-23 01:32:06 +0530

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -274,7 +274,7 @@ else:
     user_pic = None
 
 # Profile card
-colA, colB, spacer1 = st.columns([1, 1, 3])
+colA, colB, spacer1 = st.columns([1, 1, 2])
 with colA:
     if user_pic:
         st.image(user_pic, width=100)
